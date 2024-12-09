aaaaa323357c575bd0e311b0fbfc4c2eb59ef158
TheJethFX <thejethfx@gmail.com>
2024-12-08 20:07:01 -0500

    fix: Update prepublish script to replace "main" and "index" fields

--- prepublish.cjs
+++ prepublish.cjs
@@ -1,12 +1,24 @@
 const fs = require('fs');
+const path = require('path');
 
-const packageJsonPath = './package.json';
+// Resolve the path to package.json
+const packageJsonPath = path.resolve(__dirname, 'package.json');
+
+// Read and parse package.json
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-if (packageJson.publishConfig && packageJson.publishConfig.exports) {
+// Update "main" and "types" fields
+packageJson.main = './dist/index.js';
+packageJson.types = './dist/index.d.ts';
+
+// Conditionally add "exports" from "publishConfig.exports"
+if (packageJson.publishConfig?.exports) {
 	packageJson.exports = packageJson.publishConfig.exports;
-	fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-	console.log('Exports field added to package.json from publishConfig.exports');
+	console.log('Added "exports" field from publishConfig.exports.');
 } else {
-	console.log('No exports found in publishConfig, skipping.');
+	console.log('No "exports" field found in publishConfig.');
 }
+
+// Write the updated package.json back to the file
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
+console.log('Updated "main", "types", and "exports" fields in package.json.');
